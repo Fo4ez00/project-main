8986d127bd560d1f8d37c36b8b1d1273b2e316cb
Yarikdolbiev <raisindolbiev@gmail.com>
2024-06-21 11:25:30 +0300

    123

--- project/src/pages/Header.jsx
+++ project/src/pages/Header.jsx
@@ -1,21 +1,25 @@
 import "../styles/HeaderMenu.css";
 import { Link } from "react-router-dom";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 const Header = ({ user }) => {
   return (
-    <header>
+    <header className="header">
       <h1>PIZZA DAY</h1>
-      <nav>
-      <Link to="/" className="nav-link">Pizza</Link>
-      <Link to="/cart" className="nav-link">Cart</Link>
+      <nav className="nav">
+        <div className="nav-left">
+          <Link to="/" className="nav-link">Pizza</Link>
+          <Link to="/cart" className="nav-linkdelete"><AddShoppingCartIcon /></Link>
+        </div>
+        <div className="nav-right">
+          {user && <p className="user-info">{user}</p>}
+        </div>
       </nav>
       <input
         type="search"
         className="search-input_Menu"
         placeholder="Search..."
       />
-      {user && <p className="user-info">{user}</p>}
-      
     </header>
   );
 };
